7799135303c659534deaf80c5b07dcf5e6d8286e
ricksonthompson <ricksonthompson.dev@gmail.com>
2025-05-04 21:24:35 -0400

    feat: Enhance ListHeader component with improved expand/collapse button and icons

--- src/pages/CustomLists.tsx
+++ src/pages/CustomLists.tsx
@@ -403,9 +403,35 @@ const ListHeader: React.FC<ListHeaderProps> = ({
 
         <button
           onClick={() => toggleExpandList(list.id)}
-          className="text-white hover:underline"
+          className="text-white hover:underline px-2 py-1 rounded hover:bg-green-400"
         >
-          {isExpanded ? "Recolher" : "Expandir"}
+          {isExpanded ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          )}
         </button>
 
         <button
